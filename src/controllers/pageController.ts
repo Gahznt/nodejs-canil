58370542740ef8b6ec0346ad2090402fe71db98b
Gabriel Zanata <zanatag.gz@gmail.com>
2022-04-30 13:29:24 -0300

    Finalização do projeto

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from 'express';
 import { createMenuObject } from '../helpers/createMenuObject';
+import { Pet } from '../models/pet';
 
 export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll();
     res.render('pages/page', {
         menu: createMenuObject('all'),
+        list,
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
     res.render('pages/page', {
         menu: createMenuObject('dog'),
+        list,
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -22,8 +27,10 @@ export const dogs = (req: Request, res: Response) => {
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
     res.render('pages/page', {
         menu: createMenuObject('cat'),
+        list,
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -32,8 +39,10 @@ export const cats = (req: Request, res: Response) => {
 }
 
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
     res.render('pages/page', {
         menu: createMenuObject('fish'),
+        list,
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
